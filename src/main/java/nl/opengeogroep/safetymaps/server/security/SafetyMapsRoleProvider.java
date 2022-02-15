e893cfe4509ed067b896b7e2ae2f65624ba06686
Bart Verhaar <b.verhaar@safetyct.com>
2022-02-15 12:31:32 +0100

    Revert "Merge remote-tracking branch 'origin/dev/login-external-roles-improvements' into fix/smvng"
    This reverts commit 23ccbffcbe382318ca9bec3ed6ff4058eae3170c, reversing
    changes made to e042fb96a8712cd6f78965a9ec7789b037595fe6.

--- src/main/java/nl/opengeogroep/safetymaps/server/security/SafetyMapsRoleProvider.java
+++ src/main/java/nl/opengeogroep/safetymaps/server/security/SafetyMapsRoleProvider.java
@@ -5,17 +5,15 @@
  */
 package nl.opengeogroep.safetymaps.server.security;
 
-import nl.opengeogroep.safetymaps.server.db.DB;
-import org.apache.commons.dbutils.handlers.ColumnListHandler;
-import org.apache.commons.dbutils.handlers.MapListHandler;
-
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-
+import nl.opengeogroep.safetymaps.server.db.DB;
 import static nl.opengeogroep.safetymaps.server.db.DB.qr;
+import org.apache.commons.dbutils.handlers.ColumnListHandler;
+import org.apache.commons.dbutils.handlers.MapListHandler;
 
 /**
  *
@@ -24,34 +22,28 @@ import static nl.opengeogroep.safetymaps.server.db.DB.qr;
 public class SafetyMapsRoleProvider implements RoleProvider {
 
     @Override
-    public Collection<String> getRoles(String username) {
-        try {
-            return qr().query("select role from " + DB.USER_ROLE_TABLE + " where username = ?", new ColumnListHandler<String>(), username);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+    public Collection<String> getRoles(String username) throws Exception {
+
+        return qr().query("select role from " + DB.USER_ROLE_TABLE + " where username = ?", new ColumnListHandler<String>(), username);
     }
 
     @Override
-    public Map<String, Collection<String>> getAllRolesByUsername() {
-        try {
-            List<Map<String,Object>> rows = qr().query("select username, role from " + DB.USER_ROLE_TABLE, new MapListHandler());
+    public Map<String, Collection<String>> getAllRolesByUsername() throws Exception {
 
-            Map<String,Collection<String>> rolesByUsername = new HashMap();
+        List<Map<String,Object>> rows = qr().query("select username, role from " + DB.USER_ROLE_TABLE, new MapListHandler());
 
-            for(Map<String,Object> row: rows) {
-                String username = (String)row.get("username");
-                String role = (String)row.get("role");
-                Collection<String> roles = rolesByUsername.get(username);
-                if(roles == null) {
-                    roles = new HashSet();
-                    rolesByUsername.put(username, roles);
-                }
-                roles.add(role);
+        Map<String,Collection<String>> rolesByUsername = new HashMap();
+
+        for(Map<String,Object> row: rows) {
+            String username = (String)row.get("username");
+            String role = (String)row.get("role");
+            Collection<String> roles = rolesByUsername.get(username);
+            if(roles == null) {
+                roles = new HashSet();
+                rolesByUsername.put(username, roles);
             }
-            return rolesByUsername;
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+            roles.add(role);
         }
+        return rolesByUsername;
     }
 }
