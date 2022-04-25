05acd1d87625f2d1d4d251e6d7a71fd5d1b36fd1
Bart Verhaar <b.verhaar@safetyct.com>
2022-04-25 12:02:16 +0200

    Update livestream ui

---
+++ db/live.sql
@@ -0,0 +1,19 @@
+CREATE TABLE safetymaps.live (
+	incident varchar NOT NULL,
+	name varchar NOT NULL,
+	url varchar NOT NULL,
+	username varchar NULL,
+	pass varchar NULL,
+	vod bool NULL,
+	disableaudio bool NULL,
+	debug bool NULL,
+	CONSTRAINT live_pkey PRIMARY KEY (incident, url)
+);
+
+CREATE TABLE safetymaps.live_vehicles (
+	vehicle varchar NOT NULL,
+	url varchar NOT NULL,
+	username varchar NULL,
+	pass varchar NULL,
+	CONSTRAINT live_pkey PRIMARY KEY (vehicle, url)
+);
