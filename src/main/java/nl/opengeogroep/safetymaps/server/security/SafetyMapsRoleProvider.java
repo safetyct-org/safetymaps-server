7be84000691c85810f4b6bcbc0e5a2b3ed6eaa99
Bart Verhaar <b.verhaar@safetyct.com>
2022-04-28 21:35:12 +0200

    Fix build

--- src/main/java/nl/opengeogroep/safetymaps/server/security/SafetyMapsRoleProvider.java
+++ src/main/java/nl/opengeogroep/safetymaps/server/security/SafetyMapsRoleProvider.java
@@ -22,27 +22,35 @@ import org.apache.commons.dbutils.handlers.MapListHandler;
 public class SafetyMapsRoleProvider implements RoleProvider {
 
     @Override
-    public Collection<String> getRoles(String username) throws Exception {
-        return qr().query("select role from safetymaps.user_roles where username = ? union select trim(unnest(string_to_array(r.modules, ','))) as role from safetymaps.user_roles ur inner join safetymaps.role r on ur.role = r.role and coalesce(r.role, '') <> '' where ur.username = ?", new ColumnListHandler<String>(), username, username);
+    public Collection<String> getRoles(String username) {
+        try {
+            return qr().query("select role from safetymaps.user_roles where username = ? union select trim(unnest(string_to_array(r.modules, ','))) as role from safetymaps.user_roles ur inner join safetymaps.role r on ur.role = r.role and coalesce(r.role, '') <> '' where ur.username = ?", new ColumnListHandler<String>(), username, username);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
-    public Map<String, Collection<String>> getAllRolesByUsername() throws Exception {
+    public Map<String, Collection<String>> getAllRolesByUsername() {
 
-        List<Map<String,Object>> rows = qr().query("select role, username from safetymaps.user_roles union select trim(unnest(string_to_array(r.modules, ','))) as role, ur.username from safetymaps.user_roles ur inner join safetymaps.role r on ur.role = r.role and coalesce(r.role, '') <> ''", new MapListHandler());
+        try {
+            List<Map<String,Object>> rows = qr().query("select role, username from safetymaps.user_roles union select trim(unnest(string_to_array(r.modules, ','))) as role, ur.username from safetymaps.user_roles ur inner join safetymaps.role r on ur.role = r.role and coalesce(r.role, '') <> ''", new MapListHandler());
 
-        Map<String,Collection<String>> rolesByUsername = new HashMap();
+            Map<String,Collection<String>> rolesByUsername = new HashMap();
 
-        for(Map<String,Object> row: rows) {
-            String username = (String)row.get("username");
-            String role = (String)row.get("role");
-            Collection<String> roles = rolesByUsername.get(username);
-            if(roles == null) {
-                roles = new HashSet();
-                rolesByUsername.put(username, roles);
+            for(Map<String,Object> row: rows) {
+                String username = (String)row.get("username");
+                String role = (String)row.get("role");
+                Collection<String> roles = rolesByUsername.get(username);
+                if(roles == null) {
+                    roles = new HashSet();
+                    rolesByUsername.put(username, roles);
+                }
+                roles.add(role);
             }
-            roles.add(role);
+            return rolesByUsername;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
-        return rolesByUsername;
     }
 }
