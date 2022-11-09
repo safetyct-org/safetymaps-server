4a22dfab384c0614c81bfdbf47b58033f66b0fb3
Bart Verhaar <b.verhaar@safetyct.com>
2022-11-09 10:50:10 +0100

    ...

--- src/main/java/nl/opengeogroep/safetymaps/utils/SafetyConnectMessageReceiver.java
+++ src/main/java/nl/opengeogroep/safetymaps/utils/SafetyConnectMessageReceiver.java
@@ -7,6 +7,9 @@ import java.util.List;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+
+import org.apache.commons.dbutils.handlers.ScalarHandler;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.json.JSONObject;
@@ -23,6 +26,7 @@ public class SafetyConnectMessageReceiver implements ServletContextListener {
   private static final Log log = LogFactory.getLog(SafetyConnectMessageReceiver.class);
 
   private ServletContext CONTEXT;
+  private String[] RQ_SENDERS;
   private String RQ_HOST;
   private String RQ_VHOSTS;
   private String RQ_USER;
@@ -31,9 +35,7 @@ public class SafetyConnectMessageReceiver implements ServletContextListener {
   private HashMap<String, Connection> RQ_CONNECTIONS = new HashMap<String, Connection>();
   private HashMap<String, Channel> RQ_CHANNELS = new HashMap<String, Channel>();
 
-  //private static final String RQ_MB_INCIDENTS_SYNCED = "SafetyConnect.Messages.IncidentenSynchronized";
   private static final String RQ_MB_INCIDENT_CHANGED = "SafetyConnect.Messages.IncidentChanged:IIncidentChangedEvent";
-  //private static final String RQ_MB_UNITS_SYNCED = "SafetyConnect.Messages.EenhedenSynchronized";
   private static final String RQ_MB_UNIT_CHANGED = "SafetyConnect.Messages.EenheidChanged";
   private static final String RQ_MB_UNIT_MOVED = "SafetyConnect.Messages.EenheidMoved";
 
@@ -49,6 +51,7 @@ public class SafetyConnectMessageReceiver implements ServletContextListener {
       getConfigFromDb();
     } catch (Exception e) {
       log.error("Exception while exec 'getConfigFromDb()': ", e);
+      return;
     }
 
     List<String> vhosts = Arrays.asList(RQ_VHOSTS.split(","));
@@ -62,7 +65,7 @@ public class SafetyConnectMessageReceiver implements ServletContextListener {
       }
 
       try {
-        initRabbitMqChannel(vhost, RQ_MB_INCIDENT_CHANGED);
+        initRabbitMqChannel(vhost, RQ_MB_INCIDENT_CHANGED, "incident_changed");
         log.info("SafetyConnectMessageReceiver RabbitMqChannel('" + vhost + "', '" + RQ_MB_INCIDENT_CHANGED + "') initialized.");
       } catch (Exception e) {
         log.error("Exception while exec 'initRabbitMqChannel(" + vhost + ", " + RQ_MB_INCIDENT_CHANGED + ")'", e);
@@ -96,6 +99,7 @@ public class SafetyConnectMessageReceiver implements ServletContextListener {
     RQ_VHOSTS = Cfg.getSetting("safetyconnect_rq_vhost");
     RQ_USER = Cfg.getSetting("safetyconnect_rq_user");
     RQ_PASS = Cfg.getSetting("safetyconnect_rq_pass");
+    RQ_SENDERS = Cfg.getSetting("safetyconnect_rq_senders", "").split(",");
 
     if (RQ_HOST == null || RQ_USER == null || RQ_PASS == null) {
       throw new Exception("One or more required 'safetyconnect_rq' settings are empty.");
@@ -120,45 +124,112 @@ public class SafetyConnectMessageReceiver implements ServletContextListener {
     RQ_CONNECTIONS.put(vhost, rqConFac.newConnection());
   }
 
-  private void initRabbitMqChannel(String vhost, String rqMb) throws Exception {    
-    DeliverCallback dc = (consumerTag, delivery) -> {
+  private void initRabbitMqChannel(String vhost, String rqMb, String internalEvent) throws Exception {    
+    Channel channel = RQ_CONNECTIONS.get(vhost).createChannel();
+    String queueName = nameQueue(channel, rqMb, internalEvent);
+    String channelName = nameChannel(vhost, rqMb);
+    DeliverCallback dc = getDc(vhost, rqMb);
+
+    channel.basicConsume(queueName, true, dc, consumerTag -> { });
+
+    RQ_CHANNELS.put(channelName, channel);
+  }
+
+  private void handleIncidentChangedMessage(String vhost, String msgBody) {
+    JSONObject msg = new JSONObject(msgBody);
+    JSONObject incident = msg.getJSONObject("message");
+    JSONObject incidentToSave = incident;
+    String sender = incident.getString("afzender");
+    String kic = incident.getString("kindOfChange"); // updated, synchronisatie, created
+    String incidentId = incident.getString("incidentId");
+    String envId = vhost + '-' + incidentId;
+
+    if (messageIsForMe(sender) == false) {
+      return;
+    }
+    
+    try {
+      if (kic == "updated" || kic == "synchronisatie") {
+        String dbIncidentString = DB.qr().query("SELECT details FROM safetymaps.incidents WHERE source = 'sc' AND sourceenvid = ?", new ScalarHandler<String>(), envId);
+        JSONObject dbIncident = new JSONObject(dbIncidentString);
+
+        if (dbIncident != null && !incident.getString("status").equals(dbIncident.getString("status"))) { 
+          dbIncident.put("status", incident.getString("status")); 
+        }
+
+        if (dbIncident != null && !incident.getJSONObject("brwDisciplineGegevens").toString().equals(dbIncident.getJSONObject("brwDisciplineGegevens").toString())) { 
+          dbIncident.put("brwDisciplineGegevens", incident.getJSONObject("brwDisciplineGegevens")); 
+        }
+
+        if (dbIncident != null && !incident.getJSONObject("incidentLocatie").toString().equals(dbIncident.getJSONObject("incidentLocatie").toString())) { 
+          dbIncident.put("incidentLocatie", incident.getJSONObject("incidentLocatie")); 
+        }
+
+        if (dbIncident != null && !incident.getJSONArray("kladblokregels").toString().equals(dbIncident.getJSONArray("kladblokregels").toString())) { 
+          dbIncident.put("kladblokregels", incident.getJSONArray("kladblokregels")); 
+        }
+
+        if (dbIncident != null && !incident.getJSONArray("betrokkenEenheden").toString().equals(dbIncident.getJSONArray("betrokkenEenheden").toString())) { 
+          dbIncident.put("betrokkenEenheden", incident.getJSONArray("betrokkenEenheden")); 
+        }
+
+        if (dbIncident != null && !incident.getJSONArray("karakteristieken").toString().equals(dbIncident.getJSONArray("karakteristieken").toString())) { 
+          dbIncident.put("karakteristieken", incident.getJSONArray("karakteristieken")); 
+        }
+
+        if (dbIncident != null) {
+          incidentToSave = dbIncident;
+        }
+      }
+
+      DB.qr().update("INSERT INTO safetymaps.incidents (source, sourceEnv, sourceId, sourceEnvId, details) VALUES ('sc', ?, ?, ?, ?) ON CONFLICT (sourceEnvId) DO UPDATE SET details = ?", vhost, incidentId, envId, incidentToSave.toString(), incidentToSave.toString());
+    } catch (Exception e) {
+      log.error("Exception while upserting incident(" + envId + ") in database: ", e);
+    }
+  }
+
+  private DeliverCallback getDc(String vhost, String rqMb) {
+    return (consumerTag, delivery) -> {
       String msgBody = new String(delivery.getBody(), "UTF-8");
 
       if (rqMb == RQ_MB_INCIDENT_CHANGED) {
         handleIncidentChangedMessage(vhost, msgBody);
       }
     }; 
+  }
 
-    Channel channel = RQ_CONNECTIONS.get(vhost).createChannel();
-    String queueName = channel.queueDeclare().getQueue();
-    String channelName = nameChannel(vhost, rqMb);
-
-    channel.queueBind(queueName, rqMb, "");
-    channel.basicConsume(queueName, true, dc, consumerTag -> { });
-
-    RQ_CHANNELS.put(channelName, channel);
+  private String nameQueue(Channel channel, String rqMb, String event) { //"_incident-changed"
+    String name = null;
+    try {
+      name = DB.qr().query("select queuenname from safetymaps.rq where messagebus = ?", new ScalarHandler<String>(), rqMb);
+      if (name != null) {
+        return name;
+      } else {
+        name = channel.queueDeclare("Safetymaps-Server_" + StringUtils.join(RQ_SENDERS, "_") + "_" + event, true, false, false, null).getQueue();
+        channel.queueBind(name, rqMb, "");
+        DB.qr().update("INSERT INTO safetymaps.rq (queuenname, messagebus) VALUES (?, ?)", name, rqMb);
+        return name;
+      }
+    } catch (Exception e) {
+      log.error("Exception while executing nameQueue('" + rqMb + "', '" + event + "'): ", e);
+      return null;
+    }
   }
 
   private String nameChannel(String vhost, String rqMb) {
     return vhost + "-" + rqMb;
   }
 
-  private void handleIncidentChangedMessage(String vhost, String msgBody) {
-    JSONObject msg = new JSONObject(msgBody);
-    JSONObject incident = msg.getJSONObject("message");
-    String kic = incident.getString("kindOfChange"); // updated, synchronisatie, created
-    String incidentId = incident.getString("incidentId");
-    String envId = vhost + '-' + incidentId;
-    
-    try {
-      if (kic == "updated") {
+  private boolean messageIsForMe(String sender) {
+    boolean matched = false;
 
-      } else {
-        DB.qr().update("INSERT INTO safetymaps.incidents (source, sourceEnv, sourceId, sourceEnvId, details) values ('sc', ?, ?, ?, ?) on conflict (sourceEnvId) do update set details = ?", vhost, incidentId, envId, incident.toString(), incident.toString());
+    for(String rqSender: RQ_SENDERS) {
+      if (rqSender.toLowerCase() == sender.toLowerCase()) {
+        matched = true;
       }
-    } catch (Exception e) {
-      log.error("Exception while upserting incident(" + envId + ") in database: ", e);
     }
+
+    return matched;
   }
 
   private boolean isEnabled() {
@@ -167,7 +238,7 @@ public class SafetyConnectMessageReceiver implements ServletContextListener {
 
       return "true".equals(useRabbitMq);
     } catch (Exception e) {
-      log.error("Exception while while checking isEnabled(): ", e);
+      log.error("Exception while checking isEnabled(): ", e);
     }
 
     return false;
