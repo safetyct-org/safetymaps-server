f8ea1fd5ba6be4653fd4a5faa1adf1ca8d99e81a
Bart Verhaar <b.verhaar@safetyct.com>
2023-08-14 11:29:57 +0200

    Fix missing vehicle locations?

--- src/main/java/nl/opengeogroep/safetymaps/utils/SafetyConnectMessageUtil.java
+++ src/main/java/nl/opengeogroep/safetymaps/utils/SafetyConnectMessageUtil.java
@@ -12,7 +12,7 @@ public class SafetyConnectMessageUtil {
     JSONObject activeUnitFound = null;
 
     for (Map<String, Object> incidentDbRow : dbIncidents) {
-      String incidentId = (String)incidentDbRow.get("sourceid");
+      String incidentId = (String)incidentDbRow.get("number");
       String unitsString = (String)incidentDbRow.get("units");
       JSONArray units = incidentDbRow.get("units") != null ? new JSONArray(unitsString) : new JSONArray();
 
