d69adabfd8774635c00905b351b3f19cc98b0b15
Bart Verhaar <b.verhaar@safetyct.com>
2024-01-15 12:28:07 +0100

    Fix query for oiv

--- src/main/java/nl/opengeogroep/safetymaps/server/stripes/OIVActionBean.java
+++ src/main/java/nl/opengeogroep/safetymaps/server/stripes/OIVActionBean.java
@@ -80,9 +80,10 @@ public class OIVActionBean implements ActionBean {
 
   private Resolution objects() throws SQLException, NamingException {
     List<Map<String,Object>> dbks = DB.oivQr().query(
-        "select typeobject, ot.symbol_name, vo.id, formelenaam, geom, basisreg_identifier as bid, bron, bron_tabel, max_bouwlaag, min_bouwlaag " +
+        "select typeobject, ot.symbol_name, concat('data:image/png;base64,', encode(s.symbol, 'base64')) as symbol, vo.id, formelenaam, geom, basisreg_identifier as bid, bron, bron_tabel, max_bouwlaag, min_bouwlaag " +
         "from objecten.view_objectgegevens vo " +
-        "inner join objecten.object_type ot on ot.naam = vo.typeobject "
+        "inner join objecten.object_type ot on ot.naam = vo.typeobject " +
+        "inner join objecten.view_algemeen_symbols s on s.symbol_name = ot.symbol_name "
       , new MapListHandler());
     JSONArray results = new JSONArray();
 
