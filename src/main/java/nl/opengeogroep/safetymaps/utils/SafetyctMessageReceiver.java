6dab987e757c605b3d8409f50c3c863a0af65482
Bart Verhaar <b.verhaar@safetyct.com>
2024-11-20 12:03:05 +0100

    Refactor RQ connection init

--- src/main/java/nl/opengeogroep/safetymaps/utils/SafetyctMessageReceiver.java
+++ src/main/java/nl/opengeogroep/safetymaps/utils/SafetyctMessageReceiver.java
@@ -275,6 +275,15 @@ public class SafetyctMessageReceiver implements ServletContextListener {
     if (queueIsBinded) {
       channel.basicConsume(queueName, false, messageHandler, consumerTag -> { });
       RQ_CHANNELS.put(channelName, channel);
+
+      Integer dbRec = DB.qr().query("select count(*) from safetymaps.rq where queuenname = ?", new ScalarHandler<Integer>(), queueName);
+      if (dbRec == 0) {
+        LOG.info("Queue '" + queueName + "' is binded su6fully.");
+        DB.qr().update("INSERT INTO safetymaps.rq (queuenname, messagebus) VALUES (?, ?)", queueName, rqMb);
+      }
+    } else {
+      LOG.error("Queue '" + queueName + "' is NOT binded!");
+      DB.qr().update("DELETE FROM safetymaps.rq WHERE queuenname = ?", queueName);
     }
   }
 
