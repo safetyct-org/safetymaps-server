a2032efcc896dae2054109e9209fa287f6b56cb2
Bart Verhaar <b.verhaar@safetyct.com>
2025-09-30 13:54:19 +0200

    Fix oiv for vnog

--- src/main/java/nl/opengeogroep/safetymaps/server/cache/CACHE.java
+++ src/main/java/nl/opengeogroep/safetymaps/server/cache/CACHE.java
@@ -69,6 +69,7 @@ public class CACHE {
     ClearDbks();
     
     String prefix = CACHE.getOIVVersion() > 30611 ? "concat(ot.symbol_name, '_', ot.symbol_type) as symbol_name" : "symbol_name";
+    String suffix = CACHE.getOIVVersion() > 30611 ? " ot.symbol_type,": "";
 
     List<Map<String,Object>> dbks = DB.oivQr().query(
       "select vo.typeobject, " + prefix + ", vo.id, vo.formelenaam, st_astext(vo.geom) geom, basisreg_identifier as bid, vo.bron, bron_tabel, hoogste_bouwlaag, laagste_bouwlaag, st_astext(ST_Union(ST_SnapToGrid(t.geom, 0.0001))) as terrein_geom " +
@@ -76,7 +77,7 @@ public class CACHE {
       "inner join objecten.object_type ot on ot.naam = vo.typeobject " +
       "left join (select distinct object_id, pand_id, hoogste_bouwlaag, laagste_bouwlaag from objecten.mview_bouwlagen) vb on vb.object_id = vo.id and vb.pand_id = basisreg_identifier " + 
       "left join objecten.mview_terrein t on vo.id = t.object_id " +
-      " group by vo.typeobject, ot.symbol_name, ot.symbol_type, vo.id, vo.formelenaam, vo.geom, basisreg_identifier, vo.bron, bron_tabel, hoogste_bouwlaag, laagste_bouwlaag"
+      " group by vo.typeobject, ot.symbol_name,"+ suffix +" vo.id, vo.formelenaam, vo.geom, basisreg_identifier, vo.bron, bron_tabel, hoogste_bouwlaag, laagste_bouwlaag"
     , new MapListHandler());
     
     for(Map<String, Object> dbk: dbks) {
